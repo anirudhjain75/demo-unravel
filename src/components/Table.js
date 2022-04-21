0c6ee2254e370ba608ade345775193f7b460e8aa
Anirudh Jain <anirudh.jain@outlook.com>
2022-04-21 21:46:56 +0530

    add sorting to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,48 +6,51 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { TableSortLabel } from '@mui/material';
+import { Box } from '@mui/system';
+import { visuallyHidden } from '@mui/utils';
 
 function createData(eid, name, email, phoneno) {
-  return {eid, name, email, phoneno};
+  return { eid, name, email, phoneno };
 }
 
 const rows = [
-  createData(1, "meow", "meow@meow.com", "1234567890"),
-  createData(2, "meow", "meow@meow.com", "1234567890"),
-  createData(3, "meow", "meow@meow.com", "1234567890"),
-  createData(4, "meow", "meow@meow.com", "1234567890"),
-  createData(5, "meow", "meow@meow.com", "1234567890"),
+  createData(1, "Tanya", "a@meow.com", "1234567890"),
+  createData(2, "Mehru", "b@meow.com", "234567890"),
+  createData(3, "Naveen", "c@meow.com", "34567890"),
+  createData(4, "Himanshu", "d@meow.com", "4567890"),
+  createData(5, "Zangho", "e@meow.com", "567890"),
 ];
 
 const headers = [
-    {
-        title: "Employee Id",
-        key: "eid",
-        style: {
-          width: 200
-        }
-    },
-    {
-        title: "Employee Name",
-        key: "name",
-        style: {
-          width: 300
-        }
-    },
-    {
-        title: "Email",
-        key: "email",
-        style: {
-          width: 300
-        }
-    },
-    {
-        title: "Contact No",
-        key: "phoneno",
-        style: {
-          width: 300
-        }
-    },
+  {
+    title: "Employee Id",
+    key: "eid",
+    style: {
+      width: 200
+    }
+  },
+  {
+    title: "Employee Name",
+    key: "name",
+    style: {
+      width: 400
+    }
+  },
+  {
+    title: "Email",
+    key: "email",
+    style: {
+      width: 400
+    }
+  },
+  {
+    title: "Contact No",
+    key: "phoneno",
+    style: {
+      width: 400
+    }
+  },
 
 ]
 
@@ -60,39 +63,76 @@ const commonTableBodyCellStyle = {
 }
 
 export default function BasicTable() {
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('calories');
+
+  const handleSort = (headerKey) => (event) => {
+    const isAsc = orderBy === headerKey && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(headerKey);
+  }
+
   return (
-    <TableContainer sx={{width: '60%'}} component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {headers.map(((header, idx) => {
-              if (idx == 0) {
-                return <TableCell sx={{...commonHeaderStyle, ...header.style}} value={header.key}>{header.title}</TableCell>
-              }
-              return <TableCell sx={{...commonHeaderStyle, ...header.style}} align='right' value={header.key}>{header.title}</TableCell>
-            }))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.eid}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              {Object.keys(row).map((key, idx) => {
+    <React.Fragment>
+      <TableContainer sx={{ width: '60%' }} component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {headers.map(((header, idx) => {
                 if (idx == 0) {
-                  return (
-                  <TableCell sx={{...commonTableBodyCellStyle}} component="th" scope="row">
-                    {row[key]}
-                  </TableCell>
-                  )
+                  return <TableCell sx={{ ...commonHeaderStyle, ...header.style }} value={header.key}>{header.title}</TableCell>
                 }
-                return <TableCell sx={{...commonTableBodyCellStyle}} align="right">{row[key]}</TableCell>
-              })}
+                return (
+                  <React.Fragment>
+                    <TableCell sx={{ ...commonHeaderStyle, ...header.style }} align='right' value={header.key} sortDirection={orderBy === header.key ? order : false}>
+                      {header.title}
+                      <TableSortLabel
+                        active={orderBy === header.key}
+                        direction={orderBy === header.key ? order : 'asc'}
+                        onClick={handleSort(header.key)}
+                      >
+                        {header.label}
+                        {orderBy === header.key ? (
+                          <Box component="span" sx={visuallyHidden}>
+                            {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                          </Box>
+                        ) : null}
+                      </TableSortLabel>
+                    </TableCell>
+
+                  </React.Fragment>
+                )
+              }))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.sort((a, b) => {
+              if (order === 'asc') {
+                return a[orderBy] > b[orderBy] ? 1 : -1
+              } else {
+                return a[orderBy] < b[orderBy] ? 1 : -1
+              }
+            }).map((row) => (
+              <TableRow
+                key={row.eid}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                {Object.keys(row).map((key, idx) => {
+                  if (idx == 0) {
+                    return (
+                      <TableCell sx={{ ...commonTableBodyCellStyle }} component="th" scope="row">
+                        {row[key]}
+                      </TableCell>
+                    )
+                  }
+                  return <TableCell sx={{ ...commonTableBodyCellStyle }} align="right">{row[key]}</TableCell>
+                })}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </React.Fragment>
   );
 }
